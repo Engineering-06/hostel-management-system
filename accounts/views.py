7641ec0e591ee08bad536c32b20c4df6b9be2b4c
Engineering-06 <296937411+Engineering-06@users.noreply.github.com>
2026-09-11 04:30:26 +0530

    Update hostel management system

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignupForm
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = SignupForm()
+
+    return render(request, "registration/signup.html", {"form": form})
